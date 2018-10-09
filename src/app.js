ef1e5a0b8d7cb0e40288c20c3164e496354d61cf
pikasis <pikasischris@gmail.com>
2018-10-09 15:38:00 +0300

    fix task 12 module 6

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const { accounts , users , writeJSON } = require('./data');
 
 const accountRoutes = require('./routes/accounts.js');
 
-const serviceRoutes = require('./routes/services.js');
+const servicesRoutes  = require('./routes/services.js');
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({extended: true}));
 
 app.use('/account',accountRoutes);
-app.use('/services',serviceRoutes);
+app.use('/services',servicesRoutes);
 
 app.get('/', (req,res) => {
     res.render('index',{title: 'Account Summary', accounts})
